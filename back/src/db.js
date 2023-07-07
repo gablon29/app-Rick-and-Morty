f7cc0ba829b8dd604cd92a0d07e0c7a343312cb6
gabri <gabriellondero40@gmail.com>
2023-07-07 06:57:47 -0300

    commit actual

--- back/src/db.js
+++ back/src/db.js
@@ -1,15 +1,19 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_PASSWORD, DB_USER, DB_NAME } = process.env;
-const Users = require('./Users/model/Users');
+const { DB_HOSTNAME, DB_USER, DB_PASSWORD } = process.env;
 
 const sequelize = new
- Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@localhost/${DB_NAME}`, {
+ Sequelize( DB_HOSTNAME, DB_USER, DB_PASSWORD, {
     logging: false,
-    dialect: 'postgres',
+    dialect: 'mysql',
  });
 
-Users(sequelize);
+try {
+    sequelize.authenticate();
+    console.log('database connection')
+} catch (error) {
+    console.error('error al conectar')
+}
 
 
 module.exports = {
