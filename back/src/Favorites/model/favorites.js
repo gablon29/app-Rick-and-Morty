448c8d027393db94067308b8d48ada1977ec10f5
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-07-11 17:19:47 -0300

    includind new chainge

--- back/src/Favorites/model/favorites.js
+++ back/src/Favorites/model/favorites.js
@@ -2,9 +2,21 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define('Favorites', {
-        id: {
-            type: DataTypes.UUID,
-            primaryKey: true,
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            reference: {
+                model: 'Users',
+                Key: 'id'
+            }
         },
-    });
-};
+        characterId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            reference: {
+                model: 'Characters',
+                Key: 'id'
+            }
+        }
+    }) 
+}
--- back/src/db.js
+++ back/src/db.js
@@ -1,15 +1,9 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const { DB_PASSWORD, DB_USER, DB_NAME } = process.env;
-<<<<<<< HEAD
-const Users = require('./Users/model/Users');
-const Characters = require('./Characters/model/Chracters');
-const Favorites = require('./Favorites/model/favorites');
-=======
 const UsersModel = require('./Users/model/Users.js');
 const CharactersModel = require('./Characters/model/Chracters.js');
 const FavoritesModel = require('./Favorites/model/Favorites.js')
->>>>>>> 53396de8b812acc298a76ba623eebaf72b6ccf60
 
 const sequelize = new
  Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@localhost:5432/${DB_NAME}` ,{
@@ -17,21 +11,6 @@ const sequelize = new
     dialect: 'postgres',
  });
 
-<<<<<<< HEAD
- Users(sequelize);
- Characters(sequelize);
- Favorites(sequelize);
- Users.belongsToMany(Characters, {through: Favorites})
- Characters.belongsToMany(Users, { through: Favorites });
- 
- try {
-     sequelize.sync({ force: true }).then(() => {
-         console.log('database ready')
-     })
- } catch (error) {
-     console.error('connection failed')
- }
-=======
 /**
  * La funcion para definir los modelos en la DB se llama con nombres diferentes
  * para que luego se los pueda extraer con los verdaderos nombres
@@ -52,7 +31,6 @@ Characters.belongsToMany(Users, {
     through: Favorites,
     foreignKey: 'characterId',
 })
->>>>>>> 53396de8b812acc298a76ba623eebaf72b6ccf60
 
 module.exports = {
     sequelize, ...sequelize.models
