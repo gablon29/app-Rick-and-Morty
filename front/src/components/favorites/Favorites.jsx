1353b51c38fa3cdf49eba7a8f226e9f5cdd05045
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-04 21:06:51 -0300

    included function for add favorites

--- front/src/components/favorites/Favorites.jsx
+++ front/src/components/favorites/Favorites.jsx
@@ -1,32 +1,16 @@
-import React from 'react'
-// import { useSelector } from 'react-redux'
-// import Card from '../card/Card'
-// import { useEffect } from 'react'
-// import { useDispatch } from 'react-redux'
-// import './Favorites.css'
-// import { getFavorites, orderCards } from '../../redux/actions'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+
 
 const Favorites = () => {
     
-    // const favorites = useSelector((state) => state.myFavorites)
-    // const dispatch = useDispatch()
-    // useEffect(() => {
-    //     dispatch(getFavorites())
-    // }, [])
+    const { characterFavorites } = useSelector(state => state.characterFavorites); 
+    const [ favorites, setFavorites ] = useState([]);
+    const dispatch = useDispatch();
 
-    // const orderCharac = (evento) => {
-    //     dispatch(orderCards(evento.target.value))
-    //     console.log(evento.target.value)
-    // }
-
-    // const filterCards = (evento) => {
-    //     dispatch(filterCards(evento.target.value))
-    //     console.log(evento.target.value)
-    // }
-    
-  return (
-      <div className='divFavorites'>
-          {/* {
+    return (
+        <div className='divFavorites'>
+          {
               favorites.map(({ id, name, species, gender, image }) => {
                   return (
                       <Card
@@ -37,9 +21,31 @@ const Favorites = () => {
                       />
                   )
               })
-          } */}
+          }
     </div>
   )
 }
 
 export default Favorites
+
+// import { useSelector } from 'react-redux'
+// import Card from '../card/Card'
+// import { useEffect } from 'react'
+// import { useDispatch } from 'react-redux'
+// import './Favorites.css'
+// import { getFavorites, orderCards } from '../../redux/actions'
+// const favorites = useSelector((state) => state.myFavorites)
+// const dispatch = useDispatch()
+// useEffect(() => {
+//     dispatch(getFavorites())
+// }, [])
+
+// const orderCharac = (evento) => {
+//     dispatch(orderCards(evento.target.value))
+//     console.log(evento.target.value)
+// }
+
+// const filterCards = (evento) => {
+//     dispatch(filterCards(evento.target.value))
+//     console.log(evento.target.value)
+// }
